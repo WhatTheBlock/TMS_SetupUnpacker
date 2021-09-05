97261ec3760da363048faa7c78b1be21c854420f
WhatTheBlock <40543229@gm.nfu.edu.tw>
2021-09-05 22:13:43 +0800

    完成基本功能
    尚未加入自動偵錯，且安裝進度有可能不準確

---
+++ widget.cpp
@@ -0,0 +1,115 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+
+//初始化設定
+Widget::Widget(QWidget *parent) : QWidget(parent), ui(new Ui::Widget)
+{
+    QApplication::setStyle(QStyleFactory::create("Fusion")); //設定UI介面
+    ui->setupUi(this);
+    ui->background->viewport()->setCursor(Qt::ArrowCursor); //設定TextEdit的鼠標為標準箭頭
+    ui->cmdOutput->viewport()->setCursor(Qt::ArrowCursor); //設定TextEdit的鼠標為標準箭頭
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+
+void Widget::on_setInnounpPath_clicked()
+{
+    innounpPath = QFileDialog::getOpenFileName(this, QStringLiteral("選擇innounp.exe"), "/", "innounp (innounp.exe)");
+    if(innounpPath != "") {
+        innounpPath = innounpPath.replace("/","\\");
+        ui->setInnounpPath->setText(innounpPath);
+    }
+    else {
+
+    }
+}
+
+
+void Widget::on_setSetupPath_clicked()
+{
+    setupPath = QFileDialog::getOpenFileName(this, QStringLiteral("選擇TMS安裝檔"), "/", "MapleStoryVxxx.exe (*.exe)");
+    if(setupPath != "") {
+        setupPath = setupPath.replace("/","\\");
+        ui->setSetupPath->setText(setupPath);
+    }
+    else {
+
+    }
+}
+
+
+void Widget::on_setGameSetupPath_clicked()
+{
+    gameSetupPath = QFileDialog::getExistingDirectory(this, QStringLiteral("選擇遊戲安裝的路徑"), "/", QFileDialog::ShowDirsOnly);
+    if(gameSetupPath != "") {
+        gameSetupPath = gameSetupPath.replace("/","\\");
+        ui->setGameSetupPath->setText(gameSetupPath);
+    }
+    else {
+
+    }
+}
+
+void Widget::runCmd(QString cmd, bool startInstall) {
+    ui->cmdOutput->clear();
+    cmd += "\n\r";
+    encodedString = codec->fromUnicode(cmd);
+
+    process = new QProcess(this);
+    process->start("cmd");
+    process->write(encodedString.data());
+    connect(process, SIGNAL(readyReadStandardOutput()), this, SLOT(realTimeReadOut()));
+    //connect(process, SIGNAL(readyReadStandardError()), this, SLOT(realTimeReadOut()));
+    connect(process, static_cast<void(QProcess::*)(int, QProcess::ExitStatus)>(&QProcess::finished),
+        [=](int exitCode, QProcess::ExitStatus exitStatus)
+        {
+            if(startInstall) {
+
+            }
+            else {
+                QString text = ui->cmdOutput->toPlainText();
+                text = text.mid(text.indexOf("Files: ") + 7, 3);
+                //qDebug() << text;
+                ui->total->setText(text);
+                ui->cmdOutput->clear();
+                ui->progress->setMaximum(text.toInt());
+            }
+        });
+
+    process->write("exit\n\r");
+}
+
+void Widget::realTimeReadOut() {
+    QProcess *p = dynamic_cast<QProcess *>(sender());
+    QString temp_Output = QString::fromLocal8Bit(p->readAllStandardOutput());
+    //QString temp_Error = QString::fromLocal8Bit(p->readAllStandardError());
+    qDebug() << temp_Output;
+
+    ui->cmdOutput->append(temp_Output);
+    //ui->cmdOutput->append(temp_Error);
+
+    if(temp_Output.indexOf("#") != -1) {
+        QString temp = temp_Output.mid(temp_Output.indexOf("#") + 1, temp_Output.indexOf(" {") - 1);
+        ui->extracted->setText(temp);
+        ui->progress->setValue(temp.toInt());
+    }
+}
+
+
+void Widget::on_start_clicked()
+{
+    QString cmd;
+
+    //get total files
+    cmd = "\"" + innounpPath + "\" \"" + setupPath + "\"";
+    runCmd(cmd, false);
+
+    //start install
+    cmd = "\"" + innounpPath + "\" -x -d\"" + gameSetupPath + "\\\" -a \"" + setupPath + "\"";
+    runCmd(cmd, true);
+}
+
---
+++ widget.h
@@ -0,0 +1,51 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QFileDialog>
+#include <QStyleFactory>
+#include <QProcess>
+#include <QTextCodec>
+#include <QDebug>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Widget; }
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+private slots:
+    void on_setInnounpPath_clicked();
+
+    void on_setSetupPath_clicked();
+
+    void on_setGameSetupPath_clicked();
+
+    void realTimeReadOut();
+
+    void on_start_clicked();
+
+private:
+    Ui::Widget *ui;
+
+    QString innounpPath;
+    QString setupPath;
+    QString gameSetupPath;
+
+    int files;
+
+    //統一命令編碼
+    QTextCodec *codec = QTextCodec::codecForName("Big5");
+    QByteArray encodedString;
+
+    QProcess *process;
+
+    void runCmd(QString, bool);
+};
+#endif // WIDGET_H
